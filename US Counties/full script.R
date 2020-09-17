db34ae45cc3ae1a64091d048a05ea650c8644383
benjaminzito <bzito1gulls@gmail.com>
2020-09-17 19:39:54 -0400

    Update full script.R

--- US Counties/full script.R
+++ US Counties/full script.R
@@ -56,6 +56,7 @@ shapefile_df <- shapefile_df[which(shapefile_df$NAME %in% co.est2019.alldata$CTY
 
 
 # joining shapefile_df to census data by "county_name" and "STATEFP" ----
+library(tidyverse)
 
 colnames(shapefile_df)[6] <- "county_name"
 colnames(co.est2019.alldata)[7] <- "county_name"
@@ -78,13 +79,52 @@ write.csv(merged_county_data, file = "merged_us_counties.csv")
 us_counties_json <- readOGR("us_counties.geojson")
 
 # using leaflet to map
+library(leaflet)
+
+pal <- colorNumeric(
+  palette = "Blues",
+  domain = us_counties_json$merged_us_counties_POPESTIMATE2019,
+  reverse = TRUE)
+
+
 options(viewer = NULL)
 leaflet(us_counties_json) %>%
   setView(-93.65, 42.0285, zoom = 4) %>%
   addTiles() %>%
   addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = 1,
-              fillColor = ~pal(log10(us_counties_json$merged_us_counties_POPESTIMATE2019))) %>%
+              fillColor = ~pal(us_counties_json$merged_us_counties_POPESTIMATE2019)) %>%
   addLegend(position = "bottomleft", pal = pal, 
             values = ~na.omit(us_counties_json$merged_us_counties_POPESTIMATE2019),
             title = "2019 Population Estimates")
 
+# creating the table for the dashboard
+
+table_data <- merged_county_data
+table_data <- table_data[,c(6,14,26,36)]
+
+colnames(table_data)[1] <- "County"
+colnames(table_data)[2] <- "State"
+colnames(table_data)[3] <- "2019 Population"
+colnames(table_data)[4] <- "YoY Change"
+
+table_data$`2019 Population` <-  formatC(table_data$`2019 Population`,format="f", big.mark=",", digits=0)
+
+# getting duplicate counties and states - some appear more than once
+
+n_occur <- data.frame(table(table_data$County, table_data$State))
+n_occur[n_occur$Freq > 1,]
+
+which(table_data$County == "Baltimore")
+which(table_data$County == "St. Louis")
+which(table_data$County == "Fairfax")
+which(table_data$County == "Franklin" & table_data$State == "Virginia")
+which(table_data$County == "Richmond" & table_data$State == "Virginia")
+which(table_data$County == "Roanoke" & table_data$State == "Virginia")
+
+# removing the rows
+which(table_data$County == "Baltimore")
+table_data <- table_data[-c(295,1640,1639,645,2137,2136,488,
+                            1012,1011,583,2070,2071,977,2138,2139,
+                            1347,2322,2323),]
+table_data <- table_data %>%
+  arrange(State, County)
